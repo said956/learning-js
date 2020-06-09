40790bd34896c550ef3601c13e1758a6143c6b40
ldolne <dolnelaetitia@gmail.com>
2020-06-09 13:57:37 +0200

    MAJ Ex. 05-arrays

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -26,6 +26,12 @@
         "cherry",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+
+        for(let i = 0; i < fruits.length; i++)
+        {
+            console.log(fruits[i]);
+        }
+    })
 
 })();
