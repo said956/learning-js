5ecd6bea60e22b09c55f77fbab02953fe928c7bc
ldolne <dolnelaetitia@gmail.com>
2020-06-12 12:28:12 +0200

    Ex. 01-09

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -10,7 +10,21 @@
 // You will have time to focus on it later.
 
 (function() {
+	const targetElement = document.getElementById("target");
+	let textArr = targetElement.innerHTML.split("");
+	let fontSizes = ["16px", "18px", "20px", "17px", "14px"];
+	let j = 0;
 
-    // your code here
+	targetElement.innerHTML = "";
 
+	for (let i = 0; i < textArr.length; i++)
+	{
+		targetElement.innerHTML += `<span style="font-size: ${fontSizes[j]}">${textArr[i]}</span>`;
+		j++;
+
+		if (j === fontSizes.length)
+		{
+			j = 0;
+		}
+	}
 })();
